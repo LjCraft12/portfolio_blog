48c173b6596b65bfe7dd3013db97d44ad0128a23
Perry Craft <LjCraft12@gmail.com>
2017-02-11 19:30:35 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,15 +84,15 @@ Rails.application.configure do
   config.action.mailer.default_url_options = { host: 'https://blooming-mountain-21199.herokuapp.com/'}
   config.action.mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
-  }
+#   ActionMailer::Base.smtp_settings = {
+#       :address        => 'smtp.sendgrid.net',
+#       :port           => '587',
+#       :authentication => :plain,
+#       :user_name      => ENV['SENDGRID_USERNAME'],
+#       :password       => ENV['SENDGRID_PASSWORD'],
+#       :domain         => 'heroku.com',
+#       :enable_starttls_auto => true
+#   }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
